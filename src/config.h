1c5d4dd209a556041fb790536bb43ef6ad0c967d
Aviator-codes <aviator26.codes.com>
2026-02-06 01:00:28 +0530

    Hello Window

--- src/config.h
+++ src/config.h
@@ -3,3 +3,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h> 
 #include <iostream>
+#include <cstdint>
+
+constexpr int WIN_W = 800;
+constexpr int WIN_H = 600;
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,50 @@
 #include "config.h"
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main()
 {
-    std::cout << "Hello! from GitHub!\n";
+    if(!glfwInit())
+    {
+        printf("[ERROR]: Failed to initialize GLFW!\n");
+        return EXIT_FAILURE;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, "ATDE", NULL, NULL); // Arch Three Dimensional Engine
+    if(window == NULL)
+    {
+        printf("[ERROR]: Failed to create Window!\n");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        printf("[ERROR]: Failed to initialise glad!\n");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glViewport(0, 0, WIN_W, WIN_H);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }

4667f35d49d12836684fc71608af2db6cde488f0
Aviator-codes <aviator26.codes.com>
2026-02-06 01:56:51 +0530

    Inputs and Color

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "config.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main()
 {
@@ -30,11 +31,16 @@ int main()
         return EXIT_FAILURE;
     }
     glViewport(0, 0, WIN_W, WIN_H);
+    glClearColor(0.2f, 0.2980392f, 0.6627450980f, 1.0f);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-
+    
+    
     while(!glfwWindowShouldClose(window))
     {
+        processInput(window);
+        
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -47,4 +53,10 @@ int main()
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }

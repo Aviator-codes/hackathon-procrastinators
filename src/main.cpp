8e6037b3cb960d899c9e27f2eba27a19db6dadf1
Aviator-codes <aviator26.codes.com>
2026-02-06 22:38:04 +0530

    Rectangle and Wireframe mode

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,15 @@ int main()
 
     float vertices[] = 
     {
-        -0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+    };
+    uint indicies[] = 
+    {
+        0, 1, 3,
+        1, 2, 3
     };
 
     Shader shader = Shader("../src/shaders/shader.vert", "../src/shaders/shader.frag");
@@ -25,6 +31,11 @@ int main()
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    uint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -35,14 +46,13 @@ int main()
     {
         WH.processInput();
 
-        
         // Rendering
         glClear(GL_COLOR_BUFFER_BIT);
         shader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
-        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
 
         glfwSwapBuffers(WH.get());
         glfwPollEvents();
